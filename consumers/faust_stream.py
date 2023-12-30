4789a91a6d2375f0f4c926a7e23dd1e0d2d150d3
maur1 <maureen@live.no>
2023-12-30 20:14:06 +0100

    fix docker, add faust transform

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -2,7 +2,7 @@
 import logging
 
 import faust
-
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -29,29 +29,37 @@ class TransformedStation(faust.Record):
     line: str
 
 
-# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
-#   places it into a new topic with only the necessary information.
+# Define a Faust Stream that ingests data from the Kafka Connect stations topic and
+# places it into a new topic with only the necessary information.
+
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
-# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
-# TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
-# TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+# Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
+topic = app.topic("jdbc.stations", value_type=Station)
+# Define the output Kafka Topic
+out_topic = app.topic("faust.stations", partitions=1, value_type=TransformedStation)
+# Define a Faust Table
+table = app.Table(
+    "faust.stations",
+    default=int,
+    partitions=1,
+    changelog_topic=out_topic,
+)
 
-
-#
-#
-# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
+# Using Faust, transform input `Station` records into `TransformedStation` records. Note that
 # "line" is the color of the station. So if the `Station` record has the field `red` set to true,
 # then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
+
+@app.agent(topic)
+async def transform_stations(stations):
+    async for station in stations:
+        color = np.select([station.red, station.blue, station.green], ["red", "blue", "green"], default="null")
+        transformed_station = TransformedStation(
+            station_id=station.station_id,
+            station_name=station.station_name,
+            order=station.order,
+            line=str(color)
+        )
+        await out_topic.send(value=transformed_station)
 
 
 if __name__ == "__main__":
